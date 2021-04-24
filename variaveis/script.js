324caeba17dc4a993d85944c7994f90b3f44bc28
Matheus Proenca <mproenca.marketing@gmail.com>
2021-04-24 11:53:05 -0300

    Iniciando repositorio no github do curso origamid

---
+++ variaveis/script.js
@@ -0,0 +1,19 @@
+// Declarar uma variável com o seu nome
+let nome = 'Matheus';
+
+// Declarar uma variável com a sua idade
+let idade = 26;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let comidaFavorita;
+// Atribuir valor a sua comida favorita
+    comidaFavorita = 'pizza'
+// Declarar 5 variáveis diferentes sem valores
+
+let time,
+    professor,
+    pais,
+    endereço,
+    rua = 'oliveira';
+
+console.log(nome, idade, comidaFavorita, rua);
