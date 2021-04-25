add080fd4316dff76a89fd631eece5b8ca8dd33c
Matheus Proenca <mproenca.marketing@gmail.com>
2021-04-25 11:45:18 -0300

    adiciona repositorio tipo de dados

---
+++ tipos-de-dados/script-ex01.js
@@ -0,0 +1,12 @@
+let mae = 'Tânia';
+console.log(typeof mae)
+let idade = '60';
+console.log(typeof idade)
+let myage = 26;
+let fistName = 'Matheus'
+let lastname = 'Proença'
+console.log(`Meu nome e sobrenome: ${fistName} ${lastname}.`)
+let phrase = "It's time"
+console.log(phrase)
+let verificarTipoNome = typeof fistName;
+console.log (verificarTipoNome)
---
+++ tipos-de-dados/script.js
@@ -0,0 +1,14 @@
+
+// let idade = 26;
+
+// let nome = 'Matheus';
+// let sobrenome ='Proença';
+// let nomeCompleto = nome + ' ' + sobrenome
+
+// console.log(nomeCompleto);
+
+let gols = 1000
+let frase1 = 'Romário fez ' + gols + ' gols';
+let frase2 = `Romário fez ${gols} gols`; // Utilizando tamplete string
+
+console.log(frase2)
