e8c09b6298aea7e1cbc887eaa4c353fdbe4d7b95
Matheus Proenca <mproenca.marketing@gmail.com>
2021-04-26 13:44:00 -0300

    adicionando repositorio numeros operacionais

---
+++ numeros-operadores/script.js
@@ -0,0 +1,35 @@
+// let soma = 26 + 32;
+// console.log (soma)
+
+// let subtracao = 26 - 32;
+// console.log (subtracao)
+
+// let divisao = 15 / 5;
+// console.log (divisao)
+
+// let multiplicacao = 10 * 5;
+// console.log (multiplicacao)
+
+// let modulo = 10 % 5;
+// console.log (modulo)
+
+// Operadores Aritmeticos(Strings)
+
+let soma = '100' + 50;
+console.log (soma)
+let subtracao = '100' - 50;
+console.log (subtracao)
+let multiplicacao = '100' * '2';
+console.log (multiplicacao)
+let divisao = 'Comprei 10' / 2;
+console.log (divisao)
+
+
+
+
+
+
+
+
+
+
