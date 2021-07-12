22aa25a3f9b73a83cbcdeb74eb1b2964464655ca
Matheus Proença <mproenca.marketing@gmail.com>
2021-07-12 11:17:35 -0300

    adiciona navegação por tabs com javascript

--- site-AnimaisFantasticos/script.js
+++ site-AnimaisFantasticos/script.js
@@ -1,21 +1,18 @@
-// Duplique o menu e adicione ele em copy
-const menu = document.querySelector('.menu');
-const copy = document.querySelector('.copy');
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+tabContent[0].classList.add('ativo');
 
-const cloneMenu = menu.cloneNode(true);
-copy.appendChild(cloneMenu);
-
-// Selecione o primeiro DT da dl de Faq
-const faq = document.querySelector('.faq');
-const primeiroDt = faq.querySelector('dt');
-
-
-// Selecione o DD referente ao primeiro DT
-const proximoDD = primeiroDt.nextElementSibling;
-
-console.log(proximoDD);
-
-// Substitua o conteúdo html de .faq pelo de .animais
-const animais = document.querySelector('.animais');
-
-faq.innerHTML = animais.innerHTML;
+if(tabMenu.length && tabContent.length) {
+  function activeTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove('ativo');
+    })
+    tabContent[index].classList.add('ativo');
+  }
+  
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+      activeTab(index);
+    })
+  })  
+}
